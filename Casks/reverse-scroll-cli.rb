3be9dcc2e70e217bedc37522c3298145bdc398a3
Zhenye Dong <dongzhenye@gmail.com>
2026-03-02 23:12:18 +0800

    chore: update sha256 for v0.1.0 release

--- Casks/reverse-scroll-cli.rb
+++ Casks/reverse-scroll-cli.rb
@@ -1,6 +1,6 @@
 cask "reverse-scroll-cli" do
   version "0.1.0"
-  sha256 "TODO"
+  sha256 "1f277bd5422fd51760e7e1e179e35f30a48deeaca2eec8044b8e181d9f4bbc67"
 
   url "https://github.com/dongzhenye/reverse-scroll-cli/releases/download/v#{version}/ReverseScrollCLI.app.zip"
   name "ReverseScrollCLI"
